3dccf282b6d794a89c683c36337157265139395d
Shikhar-air <shikhargupta007sg@gmail.com>
2025-08-28 00:31:29 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Shikhar Gupta",
+    author_email="shikhargupta007sg@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
+    
+    
