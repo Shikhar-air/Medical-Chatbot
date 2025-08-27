6885213c0f2d11fbde3a24591d87b7da72794daf
Shikhar-air <69814943+Shikhar-air@users.noreply.github.com>
2025-08-27 22:37:50 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully!."
